1f129b1336df5ef2d7bba82c37cfdf0875e15e12
Oguz <yilmazoguz@outlook.com>
2022-03-14 16:02:29 +0300

    Create [id].js

---
+++ pages/cats/[id].js
@@ -0,0 +1,11 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import { cats } from "../../data";
+
+export default function handler(req, res) {
+    const {id} = req.query;
+    
+    const cat = cats.find(cat => cat.id === Number(id))
+
+    res.status(200).json(cat);
+}
