1ea001b5fcd4071b8a129fb4acfc55d70b89b4f6
Oguz <yilmazoguz@outlook.com>
2022-03-14 17:25:25 +0300

    Update changes

--- pages/api/cats/index.js
+++ pages/api/cats/index.js
@@ -1,29 +1,30 @@
-import React from 'react'
-import Nav from '../../../components/Nav/Nav'
-import { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import Nav from '../../../components/Nav/Nav';
 import Card from '../../../components/Card/Card';
 
 export default function index() {
-
-  const [cats, setCats] = useState([])
-
+  
+  const [cats, setCats] = useState([]);
+  
   const fetchCats = async () => {
     const response = await fetch("/api/cats");
     const data = await response.json();
     console.log(data);
     setCats(data);
-  }
-
+  };
+  
   // to call the function above as soon as it renders:
   useEffect(() => {
     fetchCats();
-  }, [input])
+  }, []);
 
   return (
-    <div>
-      <Nav />
-      {cats.map(cat => <Card name={cat.name} id={cat.id} phone={cat.phone} email={cat.email}
-        image={cat.image} />)}
-    </div>
+    <>
+      <div>
+        <Nav />
+        {cats.map(cat => <Card name={cat.name} id={cat.id} phone={cat.phone} email={cat.email}
+          image={cat.image} />)}
+      </div>
+    </>
   )
 }
