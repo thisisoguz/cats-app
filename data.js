c41398a417cde6f37b0ee97829f2ab8b8da5b9f3
Oguz <yilmazoguz@outlook.com>
2022-03-14 15:45:18 +0300

    Create data.js in root

---
+++ data.js
@@ -0,0 +1,94 @@
+// source: https://github.com/harblaith7/NextJS-Crash-Course/blob/main/data.js
+
+export const cats = [
+    {
+      id: 1,
+      name: "Sydney",
+      phone: "333-333-3333",
+      email: "laith@hotmail.com",
+      image: {
+        url: "https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGNhdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
+        alt: "beautiful cat",
+      },
+      favoured: false,
+      description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, nesciunt!",
+      color: "grey",
+      gender: "female",
+    },
+    {
+      id: 2,
+      name: "Hector",
+      phone: "3933-333-3333",
+      email: "salamonca@hotmail.com",
+      image: {
+        url: "https://images.unsplash.com/photo-1536590158209-e9d615d525e4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fGNhdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
+        alt: "beautiful cat",
+      },
+      favoured: false,
+      description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, nesciunt!",
+      color: "grey",
+      gender: "male",
+    },
+    {
+      id: 3,
+      name: "Brownie",
+      phone: "654-333-4523",
+      email: "morgan@hotmail.com",
+      image: {
+        url: "https://images.unsplash.com/photo-1606214174585-fe31582dc6ee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGNhdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
+        alt: "beautiful cat",
+      },
+      favoured: false,
+      description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, nesciunt!",
+      color: "grey",
+      gender: "female",
+    },
+    {
+      id: 4,
+      name: "Ed",
+      phone: "633-343-3533",
+      email: "hector@hotmail.com",
+      image: {
+        url: "https://images.unsplash.com/photo-1592194996308-7b43878e84a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8Y2F0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+        alt: "beautiful cat",
+      },
+      favoured: false,
+      description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, nesciunt!",
+      color: "grey",
+      gender: "male",
+    },
+    {
+      id: 5,
+      name: "Sam",
+      phone: "743-333-3233",
+      email: "Ed@hotmail.com",
+      image: {
+        url: "https://images.unsplash.com/photo-1561948955-570b270e7c36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Y2F0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+        alt: "beautiful cat",
+      },
+      favoured: false,
+      description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, nesciunt!",
+      color: "grey",
+      gender: "female",
+    },
+    {
+      id: 6,
+      name: "Felix",
+      phone: "383-333-3333",
+      email: "gath@hotmail.com",
+      image: {
+        url: "https://images.unsplash.com/photo-1561948955-570b270e7c36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Y2F0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+        alt: "beautiful cat",
+      },
+      description:
+       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, nesciunt!",
+      favoured: false,
+      color: "grey",
+      gender: "female",
+    },
+  ];
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -3,3 +3,5 @@
 export default function handler(req, res) {
   res.status(200).json({ name: 'John Doe' })
 }
+
+localhost:3000/api/hello
