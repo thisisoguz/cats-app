6595bf7bf643db4ad2a7b0dfbfbbf16940f35c55
Oguz <yilmazoguz@outlook.com>
2022-03-14 14:42:01 +0300

    Add button in jumbotron

--- components/Jumbotron/Jumbotron.js
+++ components/Jumbotron/Jumbotron.js
@@ -2,12 +2,11 @@ import React from 'react'
 
 export default function Jumbotron() {
     return (
-        <div>
-            <div class="jumbotron jumbotron-fluid">
-                <div class="container">
-                    <h1 class="display-4">Fluid jumbotron</h1>
-                    <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
-                </div>
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <h1 class="display-4">Cats World</h1>
+                <p class="lead">Find your best friend!</p>
+                <button className="btn btn-primary">Start looking!</button>
             </div>
         </div>
     )
