22f9d39a175707755706415ae3b4be7cdba66a29
Oguz <yilmazoguz@outlook.com>
2022-03-14 14:16:07 +0300

    Paste a bootstrap template of navbar within Nav.js

--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
 export default function Nav() {
-  return (
-    <div>Nav</div>
-  )
+    return (
+        <nav class="navbar navbar-light bg-light">
+            <div class="container-fluid">
+                <span class="navbar-brand mb-0 h1">Navbar</span>
+            </div>
+        </nav>
+    )
 }
