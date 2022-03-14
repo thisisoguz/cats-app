0f8f6f6a317d80f887f839b48793512b8f5a7463
Oguz <yilmazoguz@outlook.com>
2022-03-14 16:14:06 +0300

    Construct fetchCats to fetch cat data

--- pages/api/cats/index.js
+++ pages/api/cats/index.js
@@ -2,6 +2,12 @@ import React from 'react'
 import Nav from '../../../components/Nav/Nav'
 
 export default function index() {
+
+  const fetchCats = async () => {
+    const response = await fetch("/api/cats");
+    const data = await response.json();
+  }
+
   return (
     <div>
         <Nav />
