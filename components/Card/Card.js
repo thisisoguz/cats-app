221d5d8531a1d88d80f94180ae27424950ad82af
Oguz <yilmazoguz@outlook.com>
2022-03-14 15:21:10 +0300

    Give props into Card.js

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import styles from './Card.module.css'
 
-export default function Card() {
+export default function Card({name, phone, id, email, image}) {
   return (
-    <div>
-        
+    <div className={styles.Card}>
+        <div className={styles["card-header"]}>
+          <img src={image.url} alt={image.alt} className={styles["card-img"]}/>
+        </div>
+        <div>
+          <h3>{name}</h3>
+          <p>{phone}</p>
+          <p>{email}</p>
+        </div>
     </div>
   )
 }
