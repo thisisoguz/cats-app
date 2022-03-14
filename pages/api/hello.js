8db8ca52bbf9e8ca1cccd87a9314a51109b4d87d
Oguz <yilmazoguz@outlook.com>
2022-03-14 15:46:17 +0300

    Import cats into hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
-}
+import { cats } from "../../data";
 
-localhost:3000/api/hello
+export default function handler(req, res) {
+  res.status(200).json(cats)
+}
