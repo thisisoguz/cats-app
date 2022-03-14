c592e2ba94a14e87c391c6a917fc241d946fae8e
Oguz <yilmazoguz@outlook.com>
2022-03-14 14:47:37 +0300

    Import component of Nav in index.js of cats

--- pages/cats/index.js
+++ pages/cats/index.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import Nav from '../../components/Nav/Nav'
 
 export default function index() {
   return (
-    <div>cats</div>
+    <div>
+        <Nav />
+    </div>
   )
 }
