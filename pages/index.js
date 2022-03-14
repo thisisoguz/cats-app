2517ca6334da56beff54933de097b9042a854519
Oguz <yilmazoguz@outlook.com>
2022-03-14 14:20:21 +0300

    Make the import of the Nav import manually

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Nav from '../components/Nav/Nav'
 
 export default function Home() {
   return (
@@ -10,7 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
+      <main>
         <Nav />
       </main>
 
